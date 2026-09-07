982e84a0b2b3ce0dd7a5d35dc54302eea6884f6d
Mike Abrams <mike@entropiadev.com>
2026-09-07 07:10:59 +0200

    perf: inline the theme-init script

---
+++ src/_includes/js/theme-init.js
@@ -0,0 +1,9 @@
+// Persisted theme, applied before first paint (anti-flash).
+(function () {
+  try {
+    var t = localStorage.getItem("theme");
+    if (t === "light" || t === "dark") {
+      document.documentElement.setAttribute("data-theme", t);
+    }
+  } catch (e) {}
+})();
